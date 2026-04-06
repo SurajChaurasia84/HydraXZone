0f9a6735a7dd6b025523a238bf45876ca66e94df
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-06 17:17:10 +0530

    Add lib/screens/screen_constants.dart

---
+++ lib/screens/screen_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFFF4B11);
+const darkBackground = Color(0xFF0F0F0F);
+const lightBackground = Color(0xFFFFFFFF);
+const cardBackground = Color(0xFF181818);
