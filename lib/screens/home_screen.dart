5e91d411bd3573d32824a035a9fa062e4afaabf3
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-11 19:38:00 +0530

    refactor: optimize home screen structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'battle_room_screen.dart';
 import 'battle_service.dart';
+import 'coin_badge.dart';
 import 'coin_service.dart';
 import 'leaderboard_screen.dart';
 import 'navigation_controller.dart';
@@ -867,47 +868,6 @@ class _BattleRoomTile extends StatelessWidget {
   }
 }
 
-class CoinBadge extends StatelessWidget {
-  const CoinBadge({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<int>(
-      stream: CoinService.coinStream(),
-      builder: (context, snapshot) {
-        final coins = snapshot.data ?? 0;
-
-        return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-          decoration: BoxDecoration(
-            color: Theme.of(context).brightness == Brightness.dark
-                ? Colors.white.withValues(alpha: 0.08)
-                : Colors.grey.shade100,
-            borderRadius: BorderRadius.circular(999),
-            border: Border.all(color: primaryColor.withValues(alpha: 0.22)),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Icon(
-                Icons.monetization_on_rounded,
-                size: 18,
-                color: primaryColor,
-              ),
-              const SizedBox(width: 6),
-              Text(
-                '$coins',
-                style: Theme.of(
-                  context,
-                ).textTheme.titleSmall?.copyWith(fontWeight: FontWeight.w800),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
 
 class _TournamentActionSection extends StatelessWidget {
   const _TournamentActionSection();
