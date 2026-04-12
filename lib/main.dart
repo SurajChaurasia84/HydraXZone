a31c25cd9821fa077c178d43df69c1a70c4b0482
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-12 16:01:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'screens/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GoogleSignIn.instance.initialize();
   runApp(const App());
 }
