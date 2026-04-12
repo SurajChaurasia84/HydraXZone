b323ce638ad97916c53beb0154c7263f20873766
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-12 17:55:00 +0530

    Update main entry for app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'firebase_options.dart';
 import 'screens/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   await GoogleSignIn.instance.initialize();
   runApp(const App());
 }
